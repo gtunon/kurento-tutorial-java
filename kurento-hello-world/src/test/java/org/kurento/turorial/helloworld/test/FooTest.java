3a08fca3502a59c8bf6836dd987bbce1859a76c9
gtunon <gtunon@naevatec.com>
2017-02-06 23:11:01 -0800

    added test class to hello-world

---
+++ kurento-hello-world/src/test/java/org/kurento/turorial/helloworld/test/FooTest.java
@@ -0,0 +1,18 @@
+package org.kurento.turorial.helloworld.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FooTest {
+
+	@Test
+	public void testFail() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testOK() {
+		org.junit.Assert.assertTrue(true);
+	}
+}
